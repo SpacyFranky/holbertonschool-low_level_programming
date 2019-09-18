0a92f5b3c660681844641f282236f652930f673e
SpacyFranky <bigaziz@outlook.com>
2019-09-18 11:45:53 +0000

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,4 +2,4 @@
 #include <string.h>
 /* main - entry point */
 int main(void)
-{puts ("\"Programming is like building a multilingual puzzle \n"); return 0; }
+{puts ("\"Programming is like building a multilingual puzzle"); return 0; }
