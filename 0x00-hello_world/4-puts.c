cae058ff33ffb17c4c7d5b8dd6722f84fee3dd0e
SpacyFranky <bigaziz@outlook.com>
2019-09-18 11:33:55 +0000

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* main - entry point */
 int main(void)
  {
-   puts  ( "\"Programming is like building a multilingual puzzle" ); 
+   puts ( "\"Programming is like building a multilingual puzzle \n" ); 
    return(0);
  }
