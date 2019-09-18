f27943cdf8659d5d5f69d3d5006782ec1c4ef99e
SpacyFranky <bigaziz@outlook.com>
2019-09-18 21:25:17 +0000

    understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 /**
 * main - Entry point
 * hello
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
