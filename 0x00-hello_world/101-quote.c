6473c6e7a67591ce66a7d29f588d60ff73351875
SpacyFranky <bigaziz@outlook.com>
2019-09-18 20:46:47 +0000

    understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
 * main - Entry point
 *
@@ -7,7 +7,7 @@
 int main(void)
 {
 char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-int len;
+int len=57;
 write(1, msg, len);
 return (1);
 }
