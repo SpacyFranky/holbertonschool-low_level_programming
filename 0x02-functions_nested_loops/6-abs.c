9c553d463bcfb349b7af478fa7d509df244f6ecb
SpacyFranky <bigaziz@outlook.com>
2019-09-24 01:31:00 +0000

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
-* abs - check the code for Holberton School students.
-*
+* _abs - check the code for Holberton School students.
+* @n : variable
 * Return: Always 0.
 */
 int _abs(int n)
 {
 if ((n == 0) || (n == 1))
-return n;
+return (n);
 else if (n < 0)
-return -n;
-else 
-return n*n;
+return (-n);
+else
+return (n * n);
 }
