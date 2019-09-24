30f64debbab11f3b5e3568bfca18a3f92f12adbb
SpacyFranky <bigaziz@outlook.com>
2019-09-24 02:06:07 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* print_last_digit - check the code for Holberton School students.
+* @n : variable
+* Return: Always 0.
+*/
+int print_last_digit(int n)
+{
+n = n % 10;
+return (n);
+}
