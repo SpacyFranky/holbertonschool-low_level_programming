e61ce526572707c42486f6814c338ee2fb1f2444
SpacyFranky <bigaziz@outlook.com>
2019-09-24 01:50:12 +0000

    abd

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,10 +6,9 @@
 */
 int _abs(int n)
 {
-if ((n == 0) || (n == 1))
-_putchar (n + '0');
-else if (n < 0)
+if (n < 0)
 _putchar (-n + '0');
 else
-_putchar (n * n + '0');
+_putchar (n + '0');
+return (0);
 }
