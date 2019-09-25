fdb66d62d04e9c80eb5e6816d41bdc9a9c045cb8
SpacyFranky <bigaziz@outlook.com>
2019-09-25 13:46:58 +0000

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
 */
 void print_times_table(int n)
 {
-int i,j,m;
+int i, j, m;
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
@@ -41,8 +41,8 @@ else
 _putchar(',');
 _putchar(' ');
 _putchar(m / 100 + '0');
- _putchar((m % 100) / 10 + '0');
- _putchar((m % 100) % 10 + '0');
+_putchar((m % 100) / 10 + '0');
+_putchar((m % 100) % 10 + '0');
 }
 }
 }
