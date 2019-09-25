32a07cd5c6df5825e85ec6ca54969d548a7981c8
SpacyFranky <bigaziz@outlook.com>
2019-09-25 12:08:31 +0000

    update

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+/**
+* print_times_table - check the code for Holberton School students.
+* @n : varibale
+* Return: Always 0.
+*/
+void print_times_table(int n)
+{
+int i;
+int j;
+int m;
+int a;
+int b;
+if (n >= 0 && n <= 15)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+m = i * j;
+if (m == 0 && j == 0)
+{
+_putchar(m + '0');
+}
+else
+{
+if (m < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(m + '0');
+}
+else if (m < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+a = m / 100;
+b = m % 100;
+_putchar(a + '0');
+_putchar(b / 10 + '0');
+_putchar(b % 10 + '0');
+}
+}
+}
+_putchar('\n');
+}
+}
+}
