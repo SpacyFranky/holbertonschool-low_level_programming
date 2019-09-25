9c5eddfe151fca5e2a82ed82b2f36cdde47b5323
SpacyFranky <bigaziz@outlook.com>
2019-09-25 22:35:57 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point.
+* the first 50 fibonacci numbers.
+* Return: Always 0.
+*/
+int main(void)
+{
+long int i, j, s;
+i = 1;
+j = 2;
+printf("%li, ", i);
+print("%li, ", j);
+for (int k = 0 ; k < 50 ; k++)
+{
+s = i + j;
+i = j;
+j = s;
+printf("%li, ", s);
+}
+}
