2f795976f32d32b87483f9b6ed46304fca88e797
SpacyFranky <bigaziz@outlook.com>
2019-09-26 10:22:10 +0000

    numbers constitue the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+void more_numbers(void)
+{
+int i,j;
+for (i = 0 ; i <= 9 ; i++)
+{
+for (j = 0 ; j <= 14 ; j++)
+{
+if (j > 10)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+}
+_putchar('\n');
+}
+}
