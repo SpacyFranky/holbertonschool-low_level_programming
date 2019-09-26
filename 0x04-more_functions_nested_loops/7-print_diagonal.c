d4707344a9bb7b7a216e6f338ef5f48234a4387c
SpacyFranky <bigaziz@outlook.com>
2019-09-26 11:46:27 +0000

    update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+* print_line - draws a straight line in the terminal.
+* @n : variable
+* Return: nothing.
+*/
+void print_line(int n)
+{
+int i;
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+else
+{
+for (i = 0 ; i < n ; i++)
+{
+for (j = 0 ; j <= i ; j++)
+{
+if (i == j)
+{
+_putchar('\\');
+}
+else
+{
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
