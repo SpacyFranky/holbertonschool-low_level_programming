cc0b254e420ec8712ed73ae078558536d3b7574b
SpacyFranky <bigaziz@outlook.com>
2019-10-01 13:27:10 +0000

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * puts_half - check the code for Holberton School students.
+ * @str : variable.
+ * Return: Void.
+ */
+void puts_half(char *str)
+{
+  int i;
+  int l;
+  for (i = 0 ; str[i] != 0 ; i++)
+    {
+      l++;
+    }
+  if (l % 2 != 0)
+    l++;
+  for (i = l / 2 ; i <= l ; i++)
+    {
+      _putchar(str[i]);
+    }
+  _putchar('\n');
+}
