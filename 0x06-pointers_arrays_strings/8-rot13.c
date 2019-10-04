b4b5eba0a432198c040d0d5d598586484225a118
SpacyFranky <bigaziz@outlook.com>
2019-10-04 13:09:37 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * *rot13 - check the code for Holberton School students.
+ * @c : variable.
+ * Return: string c.
+ */
+char *rot13(char *c)
+{
+int i, j;
+char org[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0 ; c[i] != 0 ; i++)
+{
+for (j = 0 ; j < 53 ; j++)
+{
+if (org[j] == c[i])
+{
+c[i] = rot[j];
+break;
+}
+}
+}
+return(c);
+}
