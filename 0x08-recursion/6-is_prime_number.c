679882e12ddc747886a2549dcb33f37d822a110e
SpacyFranky <bigaziz@outlook.com>
2019-10-09 17:57:54 +0000

    optimus prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * primetest - check the code for Holberton School students.
+ * @n : variable
+ * @i : variable
+ * Return: 1 or 0.
+ */
+int prime(int n, int i)
+{
+if (n <= 1)
+return (0);
+else if (i >= n)
+return (1);
+if (n % i != 0 && i != n)
+return(prime(i + 1, n));
+else
+return (0);
+}
+
+/**
+ * is_prime_number - returns 1 if the input is a prime number otherwise 0.
+ * @n : integer
+ * Return: 1 or 0.
+ */
+int is_prime_number(int n)
+{
+return (prime(n, 2));
+}
