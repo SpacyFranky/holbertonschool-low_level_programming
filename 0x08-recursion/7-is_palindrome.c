eae66d4722618d6482c82ab9d889d464ba418f45
SpacyFranky <bigaziz@outlook.com>
2019-10-09 22:34:04 +0000

    ughughgugh

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * length - returns the length of a string.
+ * @s : string
+ * Return: length of the string.
+ */
+int length(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (1 + length(s + 1));
+}
+/**
+ * palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s : string
+ * @start : start of string
+ * @end : end of string
+ * Return: 1 or 0.
+ */
+int palindrome(char *s, int start, int end)
+{
+if (s[start] != s[end])
+return (0);
+else if (start >= end)
+return (1);
+else
+return (palindrome(s, start + 1, end + 1));
+}
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s : string
+ * Return: 1 or 0.
+ */
+int is_palindrome(char *s)
+{
+int l;
+l = length(s);
+return (palindrome(s, 0, l));
+}
