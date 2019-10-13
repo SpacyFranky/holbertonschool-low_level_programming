a8a1c879c70cfdfc700b461d57d14d217ec4397c
SpacyFranky <bigaziz@outlook.com>
2019-10-13 02:00:57 +0000

    what's my name yo?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc : argument count.
+ * @argv : argument vector.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+argc = 0;
+printf("%s\n", argv[argc]);
+return (0);
+}
