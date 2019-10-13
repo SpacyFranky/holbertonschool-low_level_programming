834c43da09aa7bbecb4046e3413ef32a9fc7ddc1
SpacyFranky <bigaziz@outlook.com>
2019-10-13 16:42:56 +0000

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc : argument count.
+ * @argv : argument vector.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+int i, s, a;
+if (argc == 1)
+{
+printf("0\n");
+return (1);
+}
+else
+{
+s = 0;
+for (i = 1 ; argv[i] != 0 ; i++)
+{
+a = atoi(argv[i]);
+if (a == 0)
+{
+printf("Error\n");
+break;
+}
+else
+s = s + a;
+}
+if (a != 0)
+printf("%d\n", s);
+return (0);
+}
+}
