6d80b42888e72e608c2ee9b5dc525e30cf39be6e
SpacyFranky <bigaziz@outlook.com>
2019-10-16 19:52:55 +0000

    malloc is tough lol part3 .....

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
 char *str_concat(char *s1, char *s2)
 {
 char *ch;
-int i, l1, l2;
+int i, j, l1, l2;
 if (s1 == '\0' && s2 == '\0')
 return (NULL);
 l1 = 0;
@@ -23,9 +23,13 @@ if (ch == NULL)
 return (NULL);
 for (i = 0 ; i < l1 ; i++)
 ch[i] = s1[i];
-for (i = 0 ; i <= l2 ; i++)
-ch[l1 + i] = s2[i];
-ch[l1 + i] = '\0';
+j = 0 ;
+for (i = l1 ; i < l1 + l2 ; i++)
+{
+ch[i] = s2[j];
+j++;
+}
+ch[i] = '\0';
 return (ch);
 free(ch);
 }
