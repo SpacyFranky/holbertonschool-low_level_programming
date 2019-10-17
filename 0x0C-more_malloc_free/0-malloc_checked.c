5845e41f4ad19d600784dae1a98f81c3b328a81d
SpacyFranky <bigaziz@outlook.com>
2019-10-17 13:09:11 +0000

    will it work???

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocates memory using malloc.
+ * @b : unsigned int.
+ * Return: 98 if failed or a pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+free(p);
+}
