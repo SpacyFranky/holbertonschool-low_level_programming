2c9f582702d3b3eefad0349e747b17aace13dfa2
SpacyFranky <bigaziz@outlook.com>
2019-10-21 16:54:32 +0000

    seriously! get one

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,10 +7,11 @@
  */
 void print_dog(struct dog *d)
 {
-if (d != 0)
-{
+if (d == 0)
+return (0);
+
 printf("Name : %s\n", d->name);
 printf("Age : %f\n", d->age);
 printf("Owner : %s\n", d->owner);
-}
+
 }
