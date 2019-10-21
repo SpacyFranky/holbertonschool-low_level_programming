eb7516f19f85c8c540c6108f4439c3c2dff26940
SpacyFranky <bigaziz@outlook.com>
2019-10-21 14:57:51 +0000

    Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_
+#define _DOG_
+int _putchar(char c);
+/**
+ * struct dog - a cute little dogo
+ * @name : dog's name
+ * @age : dog's age
+ * @owner : dog's owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* _DOG_ */
