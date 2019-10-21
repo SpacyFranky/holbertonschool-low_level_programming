ea6adea7c3f2bb2f1c8af4f04a080e10e9c5bf04
SpacyFranky <bigaziz@outlook.com>
2019-10-21 16:47:50 +0000

    seriously! get one

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints a struct dog
+ * @d : variable of struct dog.
+ * Return: nothing.
+ */
+void print_dog(struct dog *d)
+{
+if (d != 0)
+{
+printf("Name : %s\n", d->name);
+printf("Age : %f\n", d->age);
+printf("Owner : %s\n", d->owner);
+}
+}
