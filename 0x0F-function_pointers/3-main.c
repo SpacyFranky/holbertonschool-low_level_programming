eaef3c18063d5077aecb9115cefaa51499994041
SpacyFranky <bigaziz@outlook.com>
2019-10-24 12:40:10 +0000

    cool one

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - performs simple operations.
+ * @argc: size arguments.
+ * @argv: argument vecter.
+ * Return: result.
+ */
+int main(int argc, char *argv[])
+{
+int (*s)(int, int);
+int a, b;
+int result;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+s = get_op_func(argv[2]);
+if (s == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+if ((!(strcmp(argv[2], "/") == 0) || (!strcmp(argv[2], "%")) == 0) && (argv[3] == 0))
+{
+printf("Error\n");
+exit(100);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+result = s(a, b);
+printf("%d\n", result);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - sum of a and b
+ * @a: first number
+ * @b: second number
+ * Return: sum of the two integers
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_add - sub of a and b
+ * @a: first number
+ * @b: second number
+ * Return: sub of the two integers
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_add - multiplication of a and b
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of the two integers
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_add - multiplication of a and b
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of the two integers
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_add - multiplication of a and b
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of the two integers
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
