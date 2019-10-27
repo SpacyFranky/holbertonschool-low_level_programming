8ac5e7648fc9aab92ac3cd8fa9322a92497edd4c
SpacyFranky <bigaziz@outlook.com>
2019-10-27 22:35:35 +0000

    uupdate

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,13 @@
 #ifndef _HEADER_
 #define _HEADER_
+#include <stdarg.h>
+
+typedef struct stru
+{
+        char *ch;
+	void (*n)(va_list);
+} st;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
