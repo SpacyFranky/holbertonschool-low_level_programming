e4a2d3ab3728dc46b1355a9fb471c3c7e9690c53
SpacyFranky <bigaziz@outlook.com>
2019-10-31 12:45:19 +0000

    is it?

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * *add_node - adds a new node at the beginning of a list_t list
  * @head: struct
@@ -16,12 +17,16 @@ list_t *add_node(list_t **head, const char *str)
 	newnode = malloc(sizeof(size_t));
 	if (newnode == NULL)
 		return (NULL);
+	if (str != NULL)
+		s = strdup(str);
 
-	s = strdup(str);
 	i = 0;
 	while (s[i] != '\0')
 		i++;
-	newnode->str = s;
+	if (i == 0)
+		newnode->str = "(nil)";
+	else
+		newnode->str = s;
 	newnode->len = i;
 	newnode->next = *head;
 	*head = newnode;
