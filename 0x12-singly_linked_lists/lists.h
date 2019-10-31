fe632f5aa651ad592368fcb6b1384a525a64222e
SpacyFranky <bigaziz@outlook.com>
2019-10-31 11:21:54 +0000

    first task

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef _LINKED_LISTS_
+#define _LINKED_LISTS_
+#include <stdlib.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+#endif /* _LINKED_LISTS_ */
