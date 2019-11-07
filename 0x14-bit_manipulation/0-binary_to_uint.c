077f7df6449c80e863c1349b78f3959d533d06c7
SpacyFranky <bigaziz@outlook.com>
2019-11-07 13:22:02 +0000

    binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,41 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-/**
- * power2 - 2 power a
- * @a: given number.
- * Return: result.
- */
-unsigned int power2(unsigned int a)
-{
-	unsigned int i, s;
-
-	s = 1;
-	for (i = 0 ; i < a ; i++)
-		s = s * 2;
-	return (s);
-}
-/**
- * tostring - converts string to integer
- * @a: string to be converted.
- * Return: converted string.
- */
-int tostring(const char *a)
-{
-	int c, sign, offset, n;
-
-	if (a[0] == '-')
-		sign = -1;
-	if (sign == -1)
-		offset = 1;
-	else
-		offset = 0;
-	n = 0;
-	for (c = offset ; a[c] != '\0' ; c++)
-		n = n * 10 + a[c] - '0';
-	if (sign == -1)
-		n = -n;
-	return (n);
-}
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
@@ -46,26 +10,31 @@ int tostring(const char *a)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, s, y;
-	int x;
+	unsigned int s, a, c;
+	int i, len;
 
 	if (b == NULL)
 		return (0);
 
+	len = 0;
 	for (i = 0 ; b[i] != '\0' ; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		len++;
 	}
-	x = tostring(b);
-	i = 0;
+	len--;
 	s = 0;
-	while (x)
+	c = 1;
+	for (i = len ; i >= 0 ; i--)
 	{
-		y = x % 10;
-		s = s + (y * power2(i));
-		x = x / 10;
-		i++;
+		if (b[i] == '0')
+			a = 0;
+		if (b[i] == '1')
+			a = 1;
+		s = s + a * c;
+		c = c * 2;
 	}
+
 	return (s);
 }
