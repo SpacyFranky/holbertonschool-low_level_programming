73dd5f4fa2d1d5be4beedaf47fb8ea6c87bf57ca
SpacyFranky <bigaziz@outlook.com>
2019-11-07 15:45:22 +0000

    :(((

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,20 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask;
-
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	mask = n;
-	while (mask > 0)
-	{
-		if ((n & mask) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-		mask = mask >> 1;
-	}
-
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
