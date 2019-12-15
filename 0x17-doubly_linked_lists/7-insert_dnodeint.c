6d731d5f29aacd06ee00dd5b9b5fbcf95f25601c
SpacyFranky <bigaziz@outlook.com>
2019-12-15 18:00:09 +0000

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,32 +6,44 @@
  * @h: doubly linked list.
  * @idx: index.
  * @n: value.
+ * Return: the address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode, *node = *h;
+	unsigned i;
+	dlistint_t *newnode, *temp = *h;
 
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 		return (NULL);
-	newnode->n = n;
+
 	if (h == NULL)
 		return (NULL);
-	if (idx == 0)
+	else
 	{
-		newnode->next = *h;
-		*h = newnode;
-		return (newnode);
+		i = 1;
+		while (i < idx - 1 && temp != NULL)
+		{
+			temp = temp->next;
+			i++;
+		}
+		if (idx == 1)
+			add_dnodeint(&temp, n); /* insert at beginning */
+		else if (temp == /* last node */)
+			add_dnodeint_end(&temp, n); /* insert at end */
+		else if (temp != NULL)
+		{
+			newnode->n = n;
+			newnode->next = temp->next;
+			newnode->prev = temp;
+
+			if (temp->next != NULL)
+				temp->next->prev = newnode;
+
+			temp->next = newnode;
+			return (*temp);
+		}
+		else
+			return (NULL);
 	}
-	while (idx - 1 > 0 && node != NULL)
-	{
-		node = node->next;
-		idx--;
-	}
-	if (idx - 1 > 0)
-		return (NULL);
-	newnode->next = node->next;
-	newnode->prev = node->prev->prev;
-	node->next = newnode;
-	return (newnode);
 }
