0e42eb08fcd91eb4f34266c933ebe84386acc583
SpacyFranky <bigaziz@outlook.com>
2020-01-19 13:34:42 +0000

    creating an empty hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,21 +8,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *htable;
+	unsigned long int i;
 
 	htable = malloc(sizeof(hash_table_t) * size);
 	if (htable == NULL)
-	{
 		return (NULL);
-	}
+
 	htable->array = malloc(sizeof(hash_node_t) * size);
 	if (htable->array == NULL)
-	{
 		return (NULL);
-	}
+
 	if (size < 1)
-	{
 		return (NULL);
-	}
 	htable->size = size;
+
+	for (i = 0; i < size; i++)
+		htable->array[i] = NULL;
+
 	return (htable);
 }
