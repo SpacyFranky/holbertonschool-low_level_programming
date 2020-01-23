a95a8cc74fbb0e0787bb14a745371805fa287aa9
SpacyFranky <bigaziz@outlook.com>
2020-01-23 10:40:41 +0000

    adding element to hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,15 +13,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 
 	if (key[0] == '\0')
-                return (0);
-        if (ht == NULL)
-                return(0);
+		return (0);
+	if (ht == NULL)
+		return (0);
 	if (ht->array == NULL)
-		return(0);
+		return (0);
 
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
-		return(0);
+		return (0);
 
 	head = ht->array;
 	index = key_index((const unsigned char *)key, ht->size);
