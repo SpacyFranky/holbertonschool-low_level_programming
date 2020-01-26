473d6a42bf21b15419d5782bfb129643a36478cc
SpacyFranky <bigaziz@outlook.com>
2020-01-26 22:49:19 +0000

    printing a hashtable

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,7 +19,7 @@ void hash_table_print(const hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		if (head[i] != NULL)
-			printf("'%s': '%s', ",head[i]->key, head[i]->value);
+			printf("'%s': '%s', ", head[i]->key, head[i]->value);
 	}
 	printf("}\n");
 }
