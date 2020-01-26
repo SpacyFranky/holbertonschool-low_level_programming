4e9d773f0df3a427b8d5b73809ec465e1968a791
SpacyFranky <bigaziz@outlook.com>
2020-01-26 16:44:32 +0000

    fixing outputs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	tmp = head[index];
 	while (head[index])
 	{
-		if (head[index]->key == key)
+		if (strcmp(head[index]->key, key) == 0)
 		{
 			free(head[index]->value);
 			free(newnode);
