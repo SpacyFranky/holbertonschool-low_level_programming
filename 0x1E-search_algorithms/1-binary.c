dd5c25a5f0387796a3e9ce88375dd0d2d5f18e6a
SpacyFranky <bigaziz@outlook.com>
2020-04-16 22:02:52 +0000

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -49,16 +49,14 @@ int binary_search(int *array, size_t size, int value)
 		if (array[m] < value)
 		{
 			l = m + 1;
-			print_array(array, l, r + 1);
 		}
 		else if (array[m] > value)
 		{
 			r = m - 1;
-			print_array(array, l, r + 1);
 		}
 		else
 			return (m);
-
+		print_array(array, l, r + 1);
 	}
 	return (-1);
 }
